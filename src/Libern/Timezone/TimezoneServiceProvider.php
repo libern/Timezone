bc1ff0648328324b1bdfa03ed099bfd0abfb4790
Libern <libernlin@gmail.com>
2015-12-07 13:27:14 +0800

    Initial commit

---
+++ src/Libern/Timezone/TimezoneServiceProvider.php
@@ -0,0 +1,45 @@
+<?php namespace Libern\Timezone;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\AliasLoader;
+
+class TimezoneServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		AliasLoader::getInstance()->alias('Timezone', 'Libern\Timezone\Facades\Timezone');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app->bind('timezone', 'Libern\Timezone\Timezone');
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
