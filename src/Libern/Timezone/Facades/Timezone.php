360c98dd19f842b5fbfba40d4cb0b57e8311b51c
Libern <libernlin@gmail.com>
2015-12-07 13:51:30 +0800

    Improve Format

--- src/Libern/Timezone/Facades/Timezone.php
+++ src/Libern/Timezone/Facades/Timezone.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Facade;
 
-class Timezone extends Facade {
-	public static function getFacadeAccessor()
-	{
-		return 'timezone';
-	}
+class Timezone extends Facade
+{
+    public static function getFacadeAccessor()
+    {
+        return 'timezone';
+    }
 }
--- src/Libern/Timezone/TimezoneServiceProvider.php
+++ src/Libern/Timezone/TimezoneServiceProvider.php
@@ -3,43 +3,44 @@
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Foundation\AliasLoader;
 
-class TimezoneServiceProvider extends ServiceProvider {
+class TimezoneServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		AliasLoader::getInstance()->alias('Timezone', 'Libern\Timezone\Facades\Timezone');
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        AliasLoader::getInstance()->alias('Timezone', 'Libern\Timezone\Facades\Timezone');
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app->bind('timezone', 'Libern\Timezone\Timezone');
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('timezone', 'Libern\Timezone\Timezone');
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 
 }
